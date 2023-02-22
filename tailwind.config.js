5efb472c33b6d394210e8ec874c86d8bbec03e6a
Sk. Sanjidul Haque <sanjidul@xpertsolvers.com>
2023-02-22 12:01:24 +0600

    categories and category wise  json data created and products card created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#49fc93",
+        
+"secondary": "#8d90ef",
+        
+"accent": "#d8b122",
+        
+"neutral": "#1F1F2E",
+        
+"base-100": "#E6E3E8",
+        
+"info": "#6CBCD0",
+        
+"success": "#15603B",
+        
+"warning": "#905109",
+        
+"error": "#F43464",
+        },
+      },
+    ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('daisyui'),
+  ],
 }
